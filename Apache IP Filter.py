8ef1d5182bb042ea007ba33dbbe2848c00a39056
Xiao_Jin <34389622+jinzhijie@users.noreply.github.com>
2019-09-22 00:32:12 -0500

    增加了排序，更科学了一点

--- Apache IP Filter.py
+++ Apache IP Filter.py
@@ -1,24 +1,29 @@
 ﻿# Apache IP Filter 0.1 by Xiao_Jin
-# GitHub: https://github.com/jinzhijie
+# GitHub: https://github.com/jinzhijie/Apache-IP-Filter
 import os
+import platform
 
 print('\n\t\t<<  Apache IP Filter 0.1 by Xiao_Jin  >>')
-os.system('title Apache IP Filter by Xiao_Jin')
+if platform.uname()[0] == "Windows":
+    os.system('title Apache IP Filter by Xiao_Jin')
+
 # Settings
 LogFilePath = r"log.txt"  # Log File
 WhiteListPath = r"white-list.txt"
 AttackIPsFile = r"attack-ips.txt"
 
 # Program
-# LogFile = open(LogFilePath, "a")
-# LogFile.write("\n")
 LogFile = open(LogFilePath, "r+")
 Log_with_useless_parts_lists = LogFile.readlines()
+New_White_ips_list = []
+
 if os.path.isfile(WhiteListPath):
     WhiteList = open(WhiteListPath, "r+")
     White_ips_list = WhiteList.readlines()
+    for i in White_ips_list:
+        New_White_ips_list.append(i.replace("\n", ""))
 else:
-    White_ips_list = []
+    New_White_ips_list = []
 attack_ips_txt = open(AttackIPsFile, "w+")
 attack_ips_txt.write("")
 
@@ -27,19 +32,21 @@ new_ip_lists = []
 
 for i in Log_with_useless_parts_lists:
     useless_start = i.find("-")
-    useless_end = i.find('\n')
-    log_useless_part = i[useless_start - 1:useless_end + 1]
-    log_with_ip = i.replace(log_useless_part, "")
+    log_useless_part = i[useless_start - 1:-1]
+    log_with_ip = i.replace(log_useless_part, "").replace("\n", "")
     ip_lists_ori.append(log_with_ip)
 
 for i in ip_lists_ori:
-    if i not in new_ip_lists and i not in White_ips_list:
+    if i not in new_ip_lists and i not in New_White_ips_list:
         new_ip_lists.append(i)
 
 print('\t\t\tAttack count: \t' + str(len(ip_lists_ori)))
 print('\t\t\tProxy ips: \t' + str(len(new_ip_lists)))
 attack_ips_txt = open(AttackIPsFile, 'a')
-for i in new_ip_lists:
-    attack_ips_txt.write(i + "\n")
 
-print("\n[OK] " + str(len(new_ip_lists)) + r' IPs has been saved in "' + AttackIPsFile + '" successfully!')
+ips = ""
+for i in sorted(new_ip_lists, key=lambda x: len(x), reverse=False):
+    ips = ips + i + "\n"
+attack_ips_txt.write(ips)
+
+print("\n[OK] " + str(len(new_ip_lists)) + r' ips has been saved in "' + AttackIPsFile + '" successfully!')
