eb53bad053ada2ac1c62ab197f7032a7b402c2f8
Xiao_Jin <34389622+jinzhijie@users.noreply.github.com>
2019-09-22 01:31:21 -0500

    及时关闭文件，修改了部分变量名

--- Apache IP Filter.py
+++ Apache IP Filter.py
@@ -5,12 +5,12 @@ import platform
 
 print('\n\t\t<<  Apache IP Filter 0.1 by Xiao_Jin  >>')
 if platform.uname()[0] == "Windows":
-    os.system('title Apache IP Filter by Xiao_Jin')
+    os.system('title Apache IP Filter 0.1 by Xiao_Jin')
 
 # Settings
 LogFilePath = r"log.txt"  # Log File
 WhiteListPath = r"white-list.txt"
-AttackIPsFile = r"attack-ips.txt"
+IPsFile = r"ips.txt"
 
 # Program
 LogFile = open(LogFilePath, "r+")
@@ -20,12 +20,15 @@ New_White_ips_list = []
 if os.path.isfile(WhiteListPath):
     WhiteList = open(WhiteListPath, "r+")
     White_ips_list = WhiteList.readlines()
+    WhiteList.close()
     for i in White_ips_list:
         New_White_ips_list.append(i.replace("\n", ""))
 else:
     New_White_ips_list = []
-attack_ips_txt = open(AttackIPsFile, "w+")
-attack_ips_txt.write("")
+
+ips_txt = open(IPsFile, "w+")
+ips_txt.write("")
+ips_txt.close()
 
 ip_lists_ori = []
 new_ip_lists = []
@@ -42,11 +45,12 @@ for i in ip_lists_ori:
 
 print('\t\t\tAttack count: \t' + str(len(ip_lists_ori)))
 print('\t\t\tProxy ips: \t' + str(len(new_ip_lists)))
-attack_ips_txt = open(AttackIPsFile, 'a')
 
+ips_txt = open(IPsFile, 'a')
 ips = ""
 for i in sorted(new_ip_lists, key=lambda x: len(x), reverse=False):
     ips = ips + i + "\n"
-attack_ips_txt.write(ips)
+ips_txt.write(ips)
+ips_txt.close()
 
-print("\n[OK] " + str(len(new_ip_lists)) + r' ips has been saved in "' + AttackIPsFile + '" successfully!')
+print("\n[OK] " + str(len(new_ip_lists)) + r' ips has been saved in "' + IPsFile + '" successfully!')
